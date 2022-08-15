bfa56dbcda241c51ed912c6d0c4bb00527bb75f5
Fernando Laueles <laufernando18@gmail.com>
2022-08-15 17:36:55 -0500

    Add point 1-4

---
+++ js/ratings.js
@@ -0,0 +1,15 @@
+function collect_ratings() {
+    var ratings = {
+        count: 0,
+        sum: 0,
+        average: 0
+    };
+
+    let rating = 0;
+
+    rating = document.querySelectorAll('.rating');
+
+    array.forEach(element => {
+        
+    });
+}
